2076f909860e4374325f63c2b6e0337343036938
Karol Brzegowy <karolbrzegowyit@gmail.com>
2024-02-06 13:31:30 +0100

    Fix test and add readme.md

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# gif-resizer-nodejs
+# GIF Resizer
+[![Node.js](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white)](https://nodejs.org/)
+[![Mocha](https://img.shields.io/badge/Mocha-8D6748?style=for-the-badge&logo=mocha&logoColor=white)](https://mochajs.org/)
+![[License]](https://badgen.net/github/license/micromatch/micromatch)
+![GitHub last commit](https://img.shields.io/github/last-commit/kbrzegowyit/gif-resizer-nodejs.git)
+
+## About the project
+GIF Resizer is a tool that allows you to scale size (width and height) of the GIF file.
+
+## Technologies
+- Node.js
+- Mocha
+
+## Getting Started
+* Clone the repository
+```
+https://github.com/kbrzegowyit/gif-resizer-nodejs.git
+```
+* Go to the project directory and install dependencies
+```
+npm install
+```
+* Run the project
+```
+npm run start:prod <inputPath> <outputPath> <scaleFactor>
+```
--- src/tests/gif-resizer.test.ts
+++ src/tests/gif-resizer.test.ts
@@ -1,33 +1,40 @@
-import { GifFrame, GifUtil } from 'gifwrap';
+import { GifUtil } from 'gifwrap';
 import GifResizer from '../gif-resizer.js';
 import assert from 'node:assert/strict';
+import { existsSync, unlinkSync } from 'fs';
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 describe('GifResizer', () => {
-    it('should resize the gif', async () => {
-        const config = {
-            inputPath: './assets/funny-celebrate.gif',
-            outputPath: 'output.gif',
-            scaleFactor: 0.5
-        };
-        const gif = await GifUtil.read(config.inputPath);
+    describe('when the gif file is valid', () => {
+        const inputPath = path.join(__dirname, '../../assets/funny-celebrate.gif');
+        const outputPath = path.join(__dirname, '/output.gif');
 
-        await GifResizer(config);
+        console.log(inputPath);
+        console.log(outputPath);
 
-        const scaledGif = await GifUtil.read(config.outputPath);
-        assert.strictEqual(scaledGif.height, gif.height * config.scaleFactor);
-        assert.strictEqual(scaledGif.width, gif.width * config.scaleFactor);
-    });
+        afterEach(() => {
+            if (existsSync(outputPath)) {
+                unlinkSync(outputPath);
+            }
+        });
 
-    it('should throw when there is no gif file', async () => {
-        const config = {
-            inputPath: 'input.gif',
-            outputPath: 'output.gif',
-            scaleFactor: 0.5
-        };
-
-        await assert.rejects(() => GifResizer(config), {
-            name: 'Error',
-            message: 'Could not read input GIF'
+        it('should resize the gif', async () => {
+            const config = {
+                inputPath,
+                outputPath,
+                scaleFactor: 0.5
+            };
+            const gif = await GifUtil.read(config.inputPath);
+    
+            await GifResizer(config);
+    
+            const scaledGif = await GifUtil.read(config.outputPath);
+            assert.strictEqual(scaledGif.height, gif.height * config.scaleFactor);
+            assert.strictEqual(scaledGif.width, gif.width * config.scaleFactor);
         });
     });
 });
